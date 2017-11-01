ff34530d662c9b28dcc9862bfcc401fefd8c5232
Kamil Łepek <kamil.lepek94@gmail.com>
2017-11-01 23:31:14 +0100

    Added some edges detection and template to determine the shape

--- Source.cpp
+++ Source.cpp
@@ -1,25 +1,78 @@
-#include <opencv2/core.hpp>
-#include <opencv2/imgcodecs.hpp>
-#include <opencv2/highgui.hpp>
+#include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
+#include <cmath>
 #include <iostream>
+#include <windows.h>
+
 using namespace cv;
 using namespace std;
-int main(int argc, char** argv)
+
+//Convert to grayscale and find edges using canny
+Mat cannyImage(const Mat source)
 {
-	Mat src, dst;
-	src = imread("D:/image.jpg", IMREAD_COLOR); // Read the file
-	dst = src.clone();
-	if (src.empty()) // Check for invalid input
+	Mat gray;
+	cvtColor(source, gray, CV_BGR2GRAY);
+
+	Mat result;
+	Canny(gray, result, 100, 200, 5);
+
+	return result;
+}
+
+//Handle situation when the image didn't load properly
+bool handleEmptyImage(Mat source)
+{
+	if (source.empty())
 	{
-		cout << "Could not open or find the image" << std::endl;
-		return -1;
+		cout << "Image didn't load properly" << endl;
+		cout << "Shutting down application" << endl;
+		Sleep(2000);
+		return true;
 	}
-	medianBlur(src, dst, 5);
-	namedWindow("Source", WINDOW_AUTOSIZE); // Create a window for display.
-	imshow("Source", src); // Show our image inside it.
-	namedWindow("Blurred", WINDOW_AUTOSIZE); // Create a window for display.
-	imshow("Blurred", dst); // Show our image inside it.
-	waitKey(0); // Wait for a keystroke in the window
+	return false;
+}
+
+//Show first image, image with edges detected and changed image
+void showImages(Mat source, Mat edges, Mat result)
+{
+	imshow("Source", source);
+	imshow("Edges", edges);
+	imshow("Result", result);
+}
+
+//Find shapes in a given set of curves and generate changed image
+void findShapes(Mat result, vector<vector<Point>> curves)
+{
+	cout << "Curves found: " << curves.size() << endl;
+	vector<Point> approx_curve;
+	for (int i = 0; i < curves.size(); i++)
+	{
+		approxPolyDP(Mat(curves[i]), approx_curve, arcLength(Mat(curves[i]), true)*0.02, true);
+		if (fabs(contourArea(curves[i])) < 100 || !isContourConvex(approx_curve)) // ignore if curve is not convex or its relatively small
+			continue;
+		if (approx_curve.size() == 3)
+		{
+			cout << "Found triangle" << endl;
+		}
+	}
+}
+
+int main()
+{
+	string path = "C:/input/h.png";
+	Mat sourceImage = imread(path);
+	Mat resultImage = sourceImage.clone();
+	if (handleEmptyImage(sourceImage))
+		return -1;
+	Mat edgesImage = cannyImage(sourceImage);
+	
+	vector<vector<Point>> curves;
+	vector<Point> approx_curve;
+	findContours(edgesImage, curves, RETR_TREE, CHAIN_APPROX_SIMPLE);
+
+	findShapes(resultImage, curves);
+
+	showImages(sourceImage, edgesImage, resultImage);
+	waitKey(0);
 	return 0;
 }
